06c7a87ce895987e08e0258431a704616d0cff24
Alisson <alissonttfire12@gmail.com>
2025-06-26 15:54:07 -0300

    feat: adicionar declarações de módulo para arquivos SVG e MP4

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="Alisson de A de Oliveira" />
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
